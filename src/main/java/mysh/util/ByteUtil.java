9f73093371c2de393f4f3725b5ec44fce90af682
mysh <mysh@1814e7e6-893b-ef47-9934-beee976ef185>
2012-07-14 07:15:11 +0000

    ByteUtil: 快速搜索重构
    git-svn-id: https://localhost:8443/svn/MyshProjects/MyshLib@124 1814e7e6-893b-ef47-9934-beee976ef185

--- src/main/java/mysh/util/ByteUtil.java
+++ src/main/java/mysh/util/ByteUtil.java
@@ -16,6 +16,11 @@ public class ByteUtil {
 	private static final Logger log = Logger.getLogger(ByteUtil.class);
 
 	/**
+	 * 默认编码.
+	 */
+	public static final String DefaultEncode = "UTF-8";
+
+	/**
 	 * 在 content 中查找 key 的位置, 找不到返回 -1.<br/>
 	 * content == null 或 key == null 或 起始位置越界, 则返回 -1.<br/>
 	 * key 长度为 0 时返回 -1.<br/>
@@ -34,8 +39,7 @@ public class ByteUtil {
 		if (startIndex < 0)
 			startIndex = 0;
 
-		if (content == null || key == null || key.length == 0
-				|| startIndex > content.length - key.length)
+		if (content == null || key == null || key.length == 0 || startIndex > content.length - key.length)
 			return -1;
 
 		int limit = content.length - key.length + 1;
@@ -68,55 +72,87 @@ public class ByteUtil {
 	 * 在 content 中查找 key 的位置(忽略大小写), 找不到返回 -1.<br/>
 	 * 
 	 * content == null 或 key == null 或 起始位置越界, 则返回 -1.<br/>
-	 * key 长度为 0 时返回 -1.<br/>
+	 * key 长度为 0 时返回 0.<br/>
 	 * 此方法源自 String.IndexOf(char[] , int , int , char[] , int , int , int )
 	 * 
 	 * @param content
 	 *               查找源
 	 * @param contentEncode
-	 *               查找源的编码格式. 若为 null, 则认为是 UTF8.
+	 *               查找源的编码格式. 若为 null, 则使用默认编码 ByteUtil.DefaultEncode.
 	 * @param startIndex
 	 *               查找源的起始索引位置
 	 * @param key
 	 *               查找目标
 	 * 
 	 */
-	public static int findStringIndexIgnoreCase(byte[] content, String contentEncode,
-			int startIndex, String key) {
+	public static int findStringIndexIgnoreCase(byte[] content, String contentEncode, int startIndex, String key) {
 
-		if (startIndex < 0)
-			startIndex = 0;
-
-		if (content == null || key == null || key.length() == 0)
+		if (content == null || key == null)
 			return -1;
 
-		byte[] key1, key2;
+		if (key.length() == 0)
+			return 0;
+
+		byte[] keyUpperCase, keyLowerCase;
 		try {
 			if (contentEncode == null || contentEncode.length() == 0)
-				contentEncode = "UTF-8";
-			key1 = key.toUpperCase().getBytes(contentEncode);
-			key2 = key.toLowerCase().getBytes(contentEncode);
+				contentEncode = ByteUtil.DefaultEncode;
+			keyUpperCase = key.toUpperCase().getBytes(contentEncode);
+			keyLowerCase = key.toLowerCase().getBytes(contentEncode);
 		} catch (UnsupportedEncodingException e) {
 			log.error("转码到 [" + contentEncode + "] 失败.", e);
 			return -1;
 		}
 
-		if (startIndex > content.length - key1.length)
+		return ByteUtil.findStringIndexIgnoreCase(content, startIndex, keyUpperCase, keyLowerCase);
+	}
+
+	/**
+	 * 在 content 中查找关键字的位置(忽略大小写), 找不到返回 -1.<br/>
+	 * 
+	 * content == null 或 查找关键字为 null 或 起始位置越界, 则返回 -1.<br/>
+	 * 查找关键字 长度为 0 时返回 0.<br/>
+	 * 此方法源自 String.IndexOf(char[] , int , int , char[] , int , int , int )
+	 * 
+	 * @param content
+	 *               查找源
+	 * @param startIndex
+	 *               查找源的起始索引位置
+	 * @param keyUpperCase
+	 *               查找关键字的大写 byteArray
+	 * @param keyLowerCase
+	 *               查找关键字的小写 byteArray
+	 * 
+	 * @return
+	 */
+	public static int findStringIndexIgnoreCase(byte[] content, int startIndex, byte[] keyUpperCase,
+			byte[] keyLowerCase) {
+
+		if (startIndex < 0)
+			startIndex = 0;
+
+		if (content == null || keyUpperCase == null || keyLowerCase == null
+				|| keyUpperCase.length != keyLowerCase.length)
 			return -1;
 
-		int limit = content.length - key1.length + 1;
-		int keyLength = key1.length;
+		if (keyUpperCase.length == 0)
+			return 0;
+
+		if (startIndex > content.length - keyUpperCase.length)
+			return -1;
+
+		int limit = content.length - keyUpperCase.length + 1;
+		int keyLength = keyUpperCase.length;
 		int j;
 
-		byte firstKey1 = key1[0];
-		byte firstKey2 = key2[0];
+		byte firstKey1 = keyUpperCase[0];
+		byte firstKey2 = keyLowerCase[0];
 		byte tempByte;
 
 		for (int i = startIndex; i < limit; i++) {
 
 			if ((tempByte = content[i]) != firstKey1 && tempByte != firstKey2) {
-				while (++i < limit && (tempByte = content[i]) != firstKey1
-						&& tempByte != firstKey2)
+				while (++i < limit && (tempByte = content[i]) != firstKey1 && tempByte != firstKey2)
 					;
 
 				if (i == limit)
@@ -124,7 +160,7 @@ public class ByteUtil {
 			}
 
 			for (j = 1; j < keyLength
-					&& ((tempByte = content[i + j]) == key1[j] || tempByte == key2[j]); j++)
+					&& ((tempByte = content[i + j]) == keyUpperCase[j] || tempByte == keyLowerCase[j]); j++)
 				;
 			if (j == keyLength)
 				return i;
@@ -132,6 +168,5 @@ public class ByteUtil {
 		}
 
 		return -1;
-
 	}
 }
