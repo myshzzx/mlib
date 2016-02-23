3ee32c60be9bfda535e0555ea7ebef52ce642076
mysh <myshzzx@gmail.com>
2016-02-23 15:49:22 +0800

    times format and parse

--- core/src/main/java/mysh/util/Times.java
+++ core/src/main/java/mysh/util/Times.java
@@ -1,6 +1,13 @@
 package mysh.util;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 import java.util.TimeZone;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * @author Mysh
@@ -51,4 +58,129 @@ public class Times {
 		return false;
 	}
 
+	public enum Formats {
+		/**
+		 * yyyy-MM-dd
+		 */
+		Day(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+		/**
+		 * HH:mm:ss
+		 */
+		Time(DateTimeFormatter.ofPattern("HH:mm:ss")),
+		/**
+		 * HH:mm
+		 */
+		HourMin(DateTimeFormatter.ofPattern("HH:mm")),
+		/**
+		 * yyyy-MM-dd HH:mm:ss
+		 */
+		DayTime(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+		/**
+		 * yyyy-MM-dd HH:mm
+		 */
+		DayHourMin(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+
+		private DateTimeFormatter formatter;
+
+		Formats(DateTimeFormatter formatter) {
+			this.formatter = formatter;
+		}
+	}
+
+	private static final ZoneId zoneSysDefault = ZoneId.systemDefault();
+	public static final ZoneId zoneBeijing = ZoneId.of("Asia/Shanghai");
+	public static final ZoneId zoneUTC = ZoneId.of("UTC");
+	public static final ZoneId zoneTokyo = ZoneId.of("Asia/Tokyo");
+	/**
+	 * Eastern Standard Time with Day Light Saving Time, used in Canada/USA (Eastern)
+	 * <p>
+	 * <a href='http://www.cs.berkeley.edu/CT/ag4.0/appendid.htm'>Time Zone Table</a>
+	 */
+	public static final ZoneId zoneEST5EDT = ZoneId.of("EST5EDT");
+	/**
+	 * Central Standard Time with Day Light Saving Time, used in Canada/USA (Central)
+	 */
+	public static final ZoneId zoneCST6CDT = ZoneId.of("CST6CDT");
+	public static final ZoneId zoneLondon = ZoneId.of("Europe/London");
+	/**
+	 * Central Europe Time, used by most western european countries
+	 */
+	public static final ZoneId zoneCET = ZoneId.of("CET");
+
+	/**
+	 * @see #format(Object, Object, ZoneId)
+	 */
+	public static String formatNow(Object format) {
+		return format(format, Instant.ofEpochMilli(System.currentTimeMillis()), zoneSysDefault);
+	}
+
+	/**
+	 * @see #format(Object, Object, ZoneId)
+	 */
+	public static String formatNow(Object format, ZoneId zone) {
+		return format(format, Instant.ofEpochMilli(System.currentTimeMillis()), zone);
+	}
+
+	/**
+	 * @see #format(Object, Object, ZoneId)
+	 */
+	public static String format(Object format, Object time) {
+		return format(format, time, zoneSysDefault);
+	}
+
+	private static final ConcurrentHashMap<String, DateTimeFormatter> formatters = new ConcurrentHashMap<>();
+
+	/**
+	 * @param format can be {@link Formats}, {@link DateTimeFormatter}, {@link String}
+	 * @param time   can be {@link Date}, {@link TemporalAccessor}
+	 * @param zone   add zone info to time if time has no zone but need a zone to format.
+	 */
+	public static String format(Object format, Object time, ZoneId zone) {
+		requireNonNull(time, "need time");
+		requireNonNull(zone, "need zone");
+		DateTimeFormatter formatter = getProperFormatter(format);
+
+		TemporalAccessor formatTime;
+
+		if (time instanceof Date)
+			formatTime = ((Date) time).toInstant().atZone(zone);
+		else if (time instanceof Instant)
+			formatTime = ((Instant) time).atZone(zone);
+		else if (time instanceof TemporalAccessor)
+			formatTime = (TemporalAccessor) time;
+		else
+			throw new IllegalArgumentException("unsupported time type: " + time.getClass());
+
+		return formatter.format(formatTime);
+	}
+
+	private static DateTimeFormatter getProperFormatter(Object format) {
+		requireNonNull(format, "need format");
+		if (format instanceof Formats)
+			return ((Formats) format).formatter;
+		else if (format instanceof DateTimeFormatter)
+			return (DateTimeFormatter) format;
+		else if (format instanceof String)
+			return formatters.computeIfAbsent((String) format, DateTimeFormatter::ofPattern);
+		else
+			throw new IllegalArgumentException("unsupported format type: " + format.getClass());
+	}
+
+	public static LocalDate parseDay(Object format, String time) {
+		requireNonNull(time, "need time");
+		DateTimeFormatter formatter = getProperFormatter(format);
+		return LocalDate.parse(time, formatter);
+	}
+
+	public static LocalTime parseTime(Object format, String time) {
+		requireNonNull(time, "need time");
+		DateTimeFormatter formatter = getProperFormatter(format);
+		return LocalTime.parse(time, formatter);
+	}
+
+	public static LocalDateTime parseDayTime(Object format, String time) {
+		requireNonNull(time, "need time");
+		DateTimeFormatter formatter = getProperFormatter(format);
+		return LocalDateTime.parse(time, formatter);
+	}
 }
--- core/src/test/java/mysh/util/TimesTest.java
+++ core/src/test/java/mysh/util/TimesTest.java
@@ -3,6 +3,12 @@ package mysh.util;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 public class TimesTest {
 
 	@Test
@@ -11,4 +17,73 @@ public class TimesTest {
 		System.out.println(Times.isTime(13, 18));
 	}
 
+	@Test
+	public void formatDate() throws Exception {
+		Times.format("yyyy-MM", new Date());
+	}
+
+	@Test
+	public void formatInstant() throws Exception {
+		Times.format("yyyy-MM", Instant.now());
+	}
+
+	@Test
+	public void formatZoned() throws Exception {
+		Times.format("yyyy-MM", Instant.now().atZone(ZoneId.systemDefault()));
+	}
+
+	@Test
+	public void formatLocal() throws Exception {
+		Times.format("yyyy-MM", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
+	}
+
+	@Test
+	public void formatLocalDate() throws Exception {
+		Times.format("yyyy-MM", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toLocalDate());
+	}
+
+	@Test
+	public void dayLightSaving() {
+		dayLightSaving(LocalDateTime.of(2010, 8, 1, 10, 0, 0).atZone(Times.zoneBeijing));
+		System.out.println(" ------------------------ ");
+		dayLightSaving(LocalDateTime.of(2010, 1, 1, 10, 0, 0).atZone(Times.zoneBeijing));
+	}
+
+	private void dayLightSaving(ZonedDateTime time) {
+		System.out.println(Times.format(Times.Formats.DayTime, time.withZoneSameInstant(ZoneId.of("UTC"))));
+		System.out.println();
+
+		System.out.println(Times.format(Times.Formats.DayTime, time.withZoneSameInstant(ZoneId.of("SystemV/EST5"))));
+		System.out.println(Times.format(Times.Formats.DayTime, time.withZoneSameInstant(ZoneId.of("EST5EDT"))));
+		System.out.println();
+
+		System.out.println(Times.format(Times.Formats.DayTime, time.withZoneSameInstant(ZoneId.of("GMT"))));
+		System.out.println(Times.format(Times.Formats.DayTime, time.withZoneSameInstant(ZoneId.of("Europe/London"))));
+		System.out.println(Times.format(Times.Formats.DayTime, time.withZoneSameInstant(ZoneId.of("CET"))));
+	}
+
+	@Test
+	public void parseDay() {
+		System.out.println(Times.parseDay("yyyy-MM-dd", "2012-12-12"));
+	}
+
+	@Test
+	public void parseDay2() {
+		System.out.println(Times.parseDay("yyyy-MM-dd HH", "2012-12-12 12"));
+	}
+
+	@Test
+	public void parseDayTime() {
+		System.out.println(Times.parseDayTime("yyyy-MM-dd HH", "2012-12-12 12"));
+	}
+
+	@Test
+	public void parseTime() {
+		System.out.println(Times.parseTime("HH", "12"));
+	}
+
+	@Test
+	public void parseTime2() {
+		System.out.println(Times.parseTime("dd HH", "12 12"));
+	}
 }
