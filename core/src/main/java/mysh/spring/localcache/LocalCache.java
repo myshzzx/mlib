60a311ce9d6140d53c032863d92177287c2f4db9
mysh <myshzzx@gmail.com>
2018-03-15 17:52:14 +0800

    add localcache

---
+++ core/src/main/java/mysh/spring/localcache/LocalCache.java
@@ -0,0 +1,64 @@
+package mysh.spring.localcache;
+
+import com.google.common.cache.CacheBuilder;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 声明本地缓存.
+ *
+ * @author zhangzhixian<hzzhangzhixian@corp.netease.com>
+ * @since 2016/7/21
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LocalCache {
+
+    /**
+     * LRU 超时 (秒).
+     * 实际设置的时间会带20%(最多10分钟)以内随机增量, 以防止相关的缓存块同时失效.
+     * 如设置超时时间为10分钟, 则实际缓存块的 LRU 超时会被设置为 10~12 分钟内的一个随机值.
+     */
+    int lruTimeoutSec();
+
+    /**
+     * 缓存块最大尺寸. 默认1(方法无参时可省略)
+     */
+    int maxSize() default 1;
+
+    /**
+     * 生成 key 的 spEL 表达式 (单 key / 批量 key).
+     * <pre>
+     * 参数列表有以下情形的, 可省略此表达式.
+     * 注意以下规则需要用户自己保证. 担心误用的话还是乖乖写表达式吧.
+     *
+     * A. 单一 key
+     *   1. 参数只含基本类型(含封装类和String, 不含数组)
+     *   2. 参数是一个自定义类型, 可以用来当 key 的(带正确的 hashCode 和 equals 实现)
+     *   3. 含 基本类型(含封装类和String,不含数组) 和 自定义类型(toString 结果可以用来当 key 的)
+     *   4. 无参 (意味着只能存一个缓存项)
+     *
+     * B. 批量 key
+     *   规则与上述规则相同, 只是这里要考查的参数类型为 List 容器内的类型与 非 List 参数类型.
+     *   如参数表 (List&lt;String&gt;, int) 按照 (String, int) 考虑.
+     *
+     * </pre>
+     */
+    String keyExp() default "";
+
+    /**
+     * 是否批量 key. 默认 false.
+     */
+    boolean isMultiKey() default false;
+
+    /**
+     * 并发级别. 默认 4.
+     * 用于控制并发更新操作的线程数, 类似 concurrentHashMap(1.6版本) segment 数量.
+     *
+     * @see CacheBuilder#concurrencyLevel(int)
+     */
+    int concurrencyLevel() default 4;
+}
---
+++ core/src/main/java/mysh/spring/localcache/LocalCacheAspect.java
@@ -0,0 +1,174 @@
+package mysh.spring.localcache;
+
+import com.google.common.base.Joiner;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.util.concurrent.ExecutionError;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 本地缓存AOP
+ *
+ * @author zhangzhixian<hzzhangzhixian @ corp.netease.com>
+ * @since 2016/7/22
+ */
+@Aspect
+@Component
+public class LocalCacheAspect {
+	private static final Logger log = LoggerFactory.getLogger(LocalCacheAspect.class);
+	
+	@Pointcut("@annotation(LocalCache)")
+	public void local() {
+	}
+	
+	private static final Joiner keyJoiner = Joiner.on('\u0000');
+	
+	@Around("local()")
+	public Object getLocalCache(final ProceedingJoinPoint pjp) throws Throwable {
+		MethodSignature signature = (MethodSignature) pjp.getSignature();
+		Method method = pjp.getTarget().getClass().getMethod(signature.getName(), signature.getMethod().getParameterTypes());
+		Object[] args = pjp.getArgs();
+		LocalCache conf = method.getAnnotation(LocalCache.class);
+		Cache<Object, AtomicReference<?>> cache = getCache(method, conf);
+		
+		if (conf.isMultiKey()) { // multiple keys
+//            MultiKeyInvokeWrap keyInfo = parseMultiKey(conf.keyExp(), pjp.getTarget(), signature.getMethod(), args);
+			return null;
+		} else { // single key
+			Object key;
+			if (StringUtils.isEmpty(conf.keyExp())) {
+				// 自动生成 key
+				key = genKey(args);
+			} else
+				key = parseSingleKey(conf.keyExp(), method, args);
+			
+			try {
+				AtomicReference<?> resultWrapper = cache.get(key, new Callable<AtomicReference<?>>() {
+					@Override
+					public AtomicReference<?> call() throws Exception {
+						try {
+							return new AtomicReference<>(pjp.proceed());
+						} catch (Throwable t) {
+							if (t instanceof Exception)
+								throw (Exception) t;
+							else
+								throw new Exception(t);
+						}
+					}
+				});
+				return resultWrapper.get();
+			} catch (ExecutionException | UncheckedExecutionException | ExecutionError e) {
+				throw e.getCause();
+			}
+		}
+	}
+	
+	/**
+	 * 自动生成key.<br/>
+	 * 规则:<br/>
+	 * 参数表为空, key = 0.
+	 * 参数表长度为 1, key = 参数[0].
+	 * 其他情况, key = join(args, \u0000).
+	 *
+	 * @param args 参数表
+	 */
+	private Object genKey(Object[] args) {
+		Object key;
+		if (args == null || args.length == 0)
+			key = 0;
+		else if (args.length == 1)
+			key = args[0];
+		else
+			key = keyJoiner.join(args);
+		return key;
+	}
+	
+	/**
+	 * spEL parser
+	 */
+	private final SpelExpressionParser spelParser = new SpelExpressionParser();
+	/**
+	 * spEL expression map
+	 */
+	private ConcurrentMap<String, Expression> exps = new ConcurrentHashMap<>();
+	/**
+	 * method param name discover
+	 */
+	private static final DefaultParameterNameDiscoverer nameDiscoverer = new DefaultParameterNameDiscoverer();
+	
+	/**
+	 * 计算单一 key
+	 */
+	private Object parseSingleKey(String key, Method method, Object[] args) {
+		// compile spEL expression
+		Expression exp = exps.get(key);
+		if (exp == null) {
+			exp = spelParser.parseExpression(key);
+			exps.put(key, exp);
+		}
+		
+		// prepare spEL context
+		EvaluationContext ctx = new StandardEvaluationContext();
+		String[] paramNames = nameDiscoverer.getParameterNames(method);
+		for (int i = 0; i < paramNames.length; i++) {
+			ctx.setVariable(paramNames[i], args[i]);
+		}
+		
+		return exp.getValue(ctx);
+	}
+	
+	private final Map<Method, Cache<Object, AtomicReference<?>>> cacheMap = new ConcurrentHashMap<>();
+	private static final Random rnd = new Random();
+	
+	/**
+	 * 以目标方法为 key 取缓存块.
+	 * 缓存块超时时间带上 20% 以内随机增量, 以避免相关的缓存数据同时失效.
+	 */
+	private Cache<Object, AtomicReference<?>> getCache(Method method, LocalCache conf) {
+		Cache<Object, AtomicReference<?>> cache = cacheMap.get(method);
+		if (cache == null) {
+			synchronized (cacheMap) {
+				cache = cacheMap.get(method);
+				if (cache == null) {
+					Assert.isTrue(conf.lruTimeoutSec() > 0, "本地缓存超时时间要大于0, " + method);
+					Assert.isTrue(conf.maxSize() > 0, "本地缓存最大尺寸要大于0, " + method);
+					Assert.isTrue(conf.concurrencyLevel() > 0, "本地缓存并发级别要大于0, " + method);
+					int timeout = conf.lruTimeoutSec();
+					int timeoutMilli = timeout > 50 * 60 ?
+							timeout * 1000 + rnd.nextInt(600 * 1000)
+							: (int) (timeout * 1000 * (1 + rnd.nextDouble() / 5));
+					cache = CacheBuilder.newBuilder()
+							.expireAfterWrite(timeoutMilli, TimeUnit.MILLISECONDS)
+							.maximumSize(conf.maxSize())
+							.concurrencyLevel(conf.concurrencyLevel())
+							.build();
+					cacheMap.put(method, cache);
+					log.info("using-local-cache: timeout={}, maxSize={}, method={},",
+							timeoutMilli, conf.maxSize(), method);
+				}
+			}
+		}
+		return cache;
+	}
+}
