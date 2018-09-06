ae5644e967742b37c8977dac2ae6675c5544e4db
myshzzx <myshzzx@users.noreply.github.com>
2018-09-06 15:03:40 +0800

    Update Exps.java

--- core/src/main/java/mysh/util/Exps.java
+++ core/src/main/java/mysh/util/Exps.java
@@ -1,4 +1,4 @@
-package mysh.util;
+package com.taobao.rdc.nextone.task.base.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,78 +11,70 @@ import java.util.function.Function;
  * 关于异常的工具类.
  */
 public abstract class Exps {
-	private static final Logger log = LoggerFactory.getLogger(Exps.class);
-	
-	/**
-	 * 将CheckedException转换为UncheckedException.
-	 */
-	public static RuntimeException unchecked(Throwable e) {
-		if (e instanceof RuntimeException) {
-			return (RuntimeException) e;
-		} else {
-			return new RuntimeException(e);
-		}
-	}
-	
-	/**
-	 * 将ErrorStack转化为String.
-	 */
-	public static String getStackTraceAsString(Exception e) {
-		StringWriter stringWriter = new StringWriter();
-		e.printStackTrace(new PrintWriter(stringWriter));
-		return stringWriter.toString();
-	}
-	
-	/**
-	 * 判断异常是否由某些底层的异常(含本层)引起. 是则返回异常, 否返回 null.
-	 */
-	@SafeVarargs
-	public static Throwable isCausedBy(Throwable ex, Class<? extends Throwable>... causeExceptionClasses) {
-		Throwable cause = ex;
-		while (cause != null) {
-			for (Class<? extends Throwable> causeClass : causeExceptionClasses) {
-				if (causeClass.isInstance(cause)) {
-					return cause;
-				}
-			}
-			cause = cause.getCause();
-		}
-		return null;
-	}
-	
-	public interface Executable<E extends Exception> {
-		void run() throws E;
-	}
-	
-	public interface ExecutableConsumer<T, E extends Exception> {
-		void accept(T t) throws E;
-	}
-	
-	/**
-	 * 执行若有异常自动重试.
-	 *
-	 * @param comment    任务备注
-	 * @param retryTimes 重试次数
-	 * @param needRetry  为null则无条件重试
-	 * @param r          任务
-	 * @return 执行抛出的异常
-	 */
-	public static <E extends Exception> E retryOnExp(
-			String comment, int retryTimes, Function<Exception, Boolean> needRetry, Executable<E> r) {
-		int times = 0;
-		while (true) {
-			try {
-				r.run();
-				return null;
-			} catch (Exception e) {
-				if (times++ < Math.max(1, retryTimes)
-						&& (needRetry == null || needRetry.apply(e))) {
-					log.info("retry-on-exp,{},{}/{}", comment, times, retryTimes, e);
-				} else {
-					log.error("stop-retry,{},{}/{}", comment, times, retryTimes, e);
-					return (E) e;
-				}
-			}
-		}
-	}
+    private static final Logger log = LoggerFactory.getLogger(Exps.class);
+
+    /**
+     * 将CheckedException转换为UncheckedException.
+     */
+    public static RuntimeException unchecked(Throwable e) {
+        if (e instanceof RuntimeException) {
+            return (RuntimeException) e;
+        } else {
+            return new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将ErrorStack转化为String.
+     */
+    public static String getStackTraceAsString(Exception e) {
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
+    }
+
+    /**
+     * 判断异常是否由某些底层的异常(含本层)引起. 是则返回异常, 否返回 null.
+     */
+    @SafeVarargs
+    public static Throwable isCausedBy(Throwable ex, Class<? extends Throwable>... causeExceptionClasses) {
+        Throwable cause = ex;
+        while (cause != null) {
+            for (Class<? extends Throwable> causeClass : causeExceptionClasses) {
+                if (causeClass.isInstance(cause)) {
+                    return cause;
+                }
+            }
+            cause = cause.getCause();
+        }
+        return null;
+    }
+
+    /**
+     * 执行若有异常自动重试.
+     *
+     * @param comment    任务备注
+     * @param retryTimes 重试次数
+     * @param needRetry  为null则无条件重试
+     * @param r          任务
+     * @return 执行抛出的异常
+     */
+    public static <E extends Exception> E retryOnExp(
+            String comment, int retryTimes, Function<Exception, Boolean> needRetry, Try.ExpRunnable<E> r) {
+        int times = 0;
+        while (true) {
+            try {
+                r.run();
+                return null;
+            } catch (Exception e) {
+                if (times++ < Math.max(1, retryTimes)
+                        && (needRetry == null || needRetry.apply(e))) {
+                    log.info("retry-on-exp,{},{}/{}", comment, times, retryTimes, e);
+                } else {
+                    log.error("stop-retry,{},{}/{}", comment, times, retryTimes, e);
+                    return (E) e;
+                }
+            }
+        }
+    }
 }
