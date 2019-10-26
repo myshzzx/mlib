87d1c32b7ded5898ef521563e9e07c2f238d1728
mysh <myshzzx@gmail.com>
2019-10-26 21:21:13 +0800

    refactor and fix

--- core/src/main/java/mysh/util/WinAPI.java
+++ core/src/main/java/mysh/util/WinAPI.java
@@ -1,16 +1,16 @@
 package mysh.util;
 
 import com.sun.jna.Native;
-import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.Kernel32Util;
-import com.sun.jna.platform.win32.User32;
-import com.sun.jna.platform.win32.WinDef;
-import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.platform.DesktopWindow;
+import com.sun.jna.platform.WindowUtils;
+import com.sun.jna.platform.win32.*;
 import com.sun.jna.win32.W32APIOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 /**
  * Windows Util.
@@ -20,23 +20,27 @@ import java.util.Objects;
  */
 public interface WinAPI {
 	Logger log = LoggerFactory.getLogger(WinAPI.class);
-
+	
 	User32 user32 = User32.INSTANCE;
 	KernelExt kernel32 = KernelExt.INSTANCE;
-
+	
 	interface KernelExt extends Kernel32 {
 		KernelExt INSTANCE = Native.load("kernel32", KernelExt.class, W32APIOptions.DEFAULT_OPTIONS);
-
+		
 		boolean SetProcessAffinityMask(HANDLE hProcess, long dwProcessAffinityMask);
-
-		/** https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-suspendthread */
+		
+		/**
+		 * https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-suspendthread
+		 */
 		int SuspendThread(HANDLE hThread);
-
-		/** https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-resumethread */
+		
+		/**
+		 * https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-resumethread
+		 */
 		int ResumeThread(HANDLE hThread);
-
+		
 	}
-
+	
 	static String getForeGroundWindowTitle() {
 		WinDef.HWND hwnd = user32.GetForegroundWindow();
 		int len = user32.GetWindowTextLength(hwnd) + 1;
@@ -44,7 +48,7 @@ public interface WinAPI {
 		user32.GetWindowText(hwnd, title, len);
 		return Native.toString(title);
 	}
-
+	
 	/**
 	 * @param mask exp : 0b11101
 	 */
@@ -53,7 +57,7 @@ public interface WinAPI {
 		WinNT.HANDLE processHandle = kernel32.OpenProcess(WinNT.PROCESS_ALL_ACCESS, false, pid);
 		if (processHandle == null)
 			throw new RuntimeException("process not found. pid=" + pid);
-
+		
 		try {
 			kernel32.SetProcessAffinityMask(processHandle, mask);
 			chkLastError();
@@ -61,7 +65,7 @@ public interface WinAPI {
 			kernel32.CloseHandle(processHandle);
 		}
 	}
-
+	
 	/**
 	 * https://msdn.microsoft.com/en-us/library/windows/desktop/ms681381(v=vs.85).aspx
 	 */
@@ -70,8 +74,10 @@ public interface WinAPI {
 		if (!Objects.equals("The operation completed successfully.", msg))
 			throw new RuntimeException(msg);
 	}
-
-	/** If the function succeeds, the return value is the thread's previous suspend count; otherwise -1 */
+	
+	/**
+	 * If the function succeeds, the return value is the thread's previous suspend count; otherwise -1
+	 */
 	static int suspendThread(int tid) {
 		WinNT.HANDLE handle = kernel32.OpenThread(
 				WinNT.THREAD_ALL_ACCESS, false, tid);
@@ -79,7 +85,7 @@ public interface WinAPI {
 		kernel32.CloseHandle(handle);
 		return r;
 	}
-
+	
 	/**
 	 * If the function succeeds, the return value is the thread's previous suspend count.
 	 * If the function fails, the return value is (DWORD) -1. To get extended error information, call GetLastError.
@@ -90,5 +96,16 @@ public interface WinAPI {
 		int r = kernel32.ResumeThread(handle);
 		kernel32.CloseHandle(handle);
 		return r;
+	}
+	
+	static void showOrHideWindow(Predicate<DesktopWindow> filter, boolean show) {
+		List<DesktopWindow> allWindows = WindowUtils.getAllWindows(!show);
+		allWindows.stream().filter(filter)
+		          .forEach(w -> showOrHideHwnd(w.getHWND(), show));
+	}
+	
+	static void showOrHideHwnd(WinDef.HWND hwnd, boolean show) {
+		if (hwnd != null)
+			user32.ShowWindow(hwnd, show ? User32.SW_SHOW : User32.SW_HIDE);
 	}
 }
