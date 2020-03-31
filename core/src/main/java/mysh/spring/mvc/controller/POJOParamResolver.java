e6f18128a1b79a3c00e948d1981445de7c050296
mysh <myshzzx@gmail.com>
2020-04-01 01:40:13 +0800

    extract Converter

--- core/src/main/java/mysh/spring/mvc/controller/POJOParamResolver.java
+++ core/src/main/java/mysh/spring/mvc/controller/POJOParamResolver.java
@@ -1,5 +1,6 @@
 package mysh.spring.mvc.controller;
 
+import mysh.util.Converter;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -8,8 +9,6 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -18,21 +17,21 @@ import java.util.*;
  * Time: 13-5-29 上午8:46
  */
 public class POJOParamResolver implements HandlerMethodArgumentResolver {
-
+	
 	@Override
 	public boolean supportsParameter(MethodParameter param) {
 		return param.getParameterAnnotation(mysh.spring.mvc.controller.POJOResolve.class) != null;
 	}
-
+	
 	@Override
 	public Object resolveArgument(MethodParameter param, ModelAndViewContainer mav,
 	                              NativeWebRequest req, WebDataBinderFactory bf) throws Exception {
-
+		
 		mysh.spring.mvc.controller.POJOResolve anno = param.getParameterAnnotation(mysh.spring.mvc.controller.POJOResolve.class);
 		String paramPrefix = (anno.value() == null || anno.value().isEmpty() ? param.getParameterName() : anno.value());
-
+		
 		Object obj = param.getParameterType().getConstructor().newInstance();
-
+		
 		String[] paramPath;
 		for (Map.Entry<String, String[]> e : req.getParameterMap().entrySet()) {
 			paramPath = e.getKey().split("\\.");
@@ -40,14 +39,14 @@ public class POJOParamResolver implements HandlerMethodArgumentResolver {
 				this.setProperty(paramPath, 1, obj, e.getValue());
 			}
 		}
-
+		
 		if (anno.validate()) {
-
+		
 		}
-
+		
 		return obj;
 	}
-
+	
 	/**
 	 * 为 path 描述的 obj 递归属性设置值.<br/>
 	 * 私有方法，为执行效率的关系不做参数有效性检查。
@@ -60,16 +59,16 @@ public class POJOParamResolver implements HandlerMethodArgumentResolver {
 	private void setProperty(String[] path, int i, Object obj, String[] value) throws Exception {
 		if ("class".equals(path[i]))
 			throw new RuntimeException("无效属性:" + path[i]);
-
+		
 		final PropertyDescriptor desc = new PropertyDescriptor(path[i], obj.getClass());
 		Method readMethod = desc.getReadMethod();
 		Method writeMethod = desc.getWriteMethod();
 		Class<?> type = readMethod.getReturnType();
-
+		
 		if (i == path.length - 1) {
-
+			
 			Object paramValue = null;
-
+			
 			// 是多值
 			if (value.length > 1) {
 				if (Collection.class.isAssignableFrom(type)) { // 是集合属性
@@ -88,33 +87,10 @@ public class POJOParamResolver implements HandlerMethodArgumentResolver {
 					throw new RuntimeException("集合值装配失败：" + Arrays.toString(path) + " = " + Arrays.toString(value));
 				}
 			}
-
+			
 			String tValue = value[0];
-
-			// 是基本类型
-			if (type == String.class) {
-				paramValue = tValue;
-			} else if (type == int.class || type == Integer.class) {
-				paramValue = Integer.valueOf(tValue);
-			} else if (type == long.class || type == Long.class) {
-				paramValue = Long.valueOf(tValue);
-			} else if (type == double.class || type == Double.class) {
-				paramValue = Double.valueOf(tValue);
-			} else if (type == byte.class || type == Byte.class) {
-				paramValue = Byte.valueOf(tValue);
-			} else if (type == short.class || type == Short.class) {
-				paramValue = Short.valueOf(tValue);
-			} else if (type == float.class || type == Float.class) {
-				paramValue = Float.valueOf(tValue);
-			}
-
-			// 是时间值
-			if (Date.class.isAssignableFrom(type)) {
-				DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
-				paramValue = type.getConstructor().newInstance();
-				((Date) paramValue).setTime(dateFormat.parse(tValue).getTime());
-			}
-
+			paramValue = Converter.stringConvert(type, tValue);
+			
 			if (paramValue == null) {
 				throw new RuntimeException("不支持的装配类型：" + type.toString());
 			}
---
+++ core/src/main/java/mysh/util/Converter.java
@@ -0,0 +1,39 @@
+package mysh.util;
+
+import java.time.ZoneOffset;
+import java.util.Date;
+
+/**
+ * @author mysh
+ * @since 2020-04-01
+ */
+public abstract class Converter {
+	public static Object stringConvert(Class<?> type, String value) throws Exception {
+		
+		// 是基本类型
+		if (type == String.class) {
+			return value;
+		} else if (type == int.class || type == Integer.class) {
+			return Integer.valueOf(value);
+		} else if (type == long.class || type == Long.class) {
+			return Long.valueOf(value);
+		} else if (type == double.class || type == Double.class) {
+			return Double.valueOf(value);
+		} else if (type == byte.class || type == Byte.class) {
+			return Byte.valueOf(value);
+		} else if (type == short.class || type == Short.class) {
+			return Short.valueOf(value);
+		} else if (type == float.class || type == Float.class) {
+			return Float.valueOf(value);
+		}
+		
+		// 是时间值
+		if (Date.class.isAssignableFrom(type)) {
+			Date d = (Date) type.getConstructor().newInstance();
+			d.setTime(Times.parseDayTime(Times.Formats.DayTime, value).atZone(ZoneOffset.systemDefault()).toEpochSecond() * 1000);
+			return d;
+		}
+		
+		return null;
+	}
+}
