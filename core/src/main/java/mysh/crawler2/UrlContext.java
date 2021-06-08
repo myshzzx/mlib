77c8cd042bfc0ef289b113a02d97fa4a7b24d3dc
凯泓 <zhixian.zzx@alibaba-inc.com>
2021-06-08 11:45:24 +0800

    make UrlContext interface

--- core/src/main/java/mysh/crawler2/UrlContext.java
+++ core/src/main/java/mysh/crawler2/UrlContext.java
@@ -6,16 +6,14 @@ import java.io.Serializable;
  * WARNING: subtypes should rewrite {@link Object#equals} and {@link Object#hashCode}
  * to prevent same tasks from been submit to crawler task. {@link lombok.Data} can be simply used.
  */
-public class UrlContext implements Serializable, Comparable<Object> {
-	
-	private static final long serialVersionUID = 1819236568182342681L;
+public interface UrlContext extends Serializable, Comparable<Object> {
 	
 	/**
 	 * to change crawler executor handling order, rewrite this method.
 	 * handle by the {@link UrlContext} sorting order.
 	 */
 	@Override
-	public int compareTo(Object o) {
+	default int compareTo(Object o) {
 		return 0;
 	}
 }
