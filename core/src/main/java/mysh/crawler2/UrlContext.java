66022717770aaea441578095a124b334ab888f99
mysh <myshzzx@gmail.com>
2021-06-08 10:16:40 +0800

    fix

--- core/src/main/java/mysh/crawler2/UrlContext.java
+++ core/src/main/java/mysh/crawler2/UrlContext.java
@@ -2,6 +2,10 @@ package mysh.crawler2;
 
 import java.io.Serializable;
 
+/**
+ * WARNING: subtypes should rewrite {@link Object#equals} and {@link Object#hashCode}
+ * to prevent same tasks from been submit to crawler task. {@link lombok.Data} can be simply used.
+ */
 public class UrlContext implements Serializable, Comparable<Object> {
 	
 	private static final long serialVersionUID = 1819236568182342681L;
